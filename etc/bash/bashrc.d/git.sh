d7e911b2cf2ff7a6feba13608ebc71233c40eaa7
wangjiezhe <wangjiezhe@gmail.com>
2024-01-18 12:20:17 +0800

    update

--- etc/bash/bashrc.d/git.sh
+++ etc/bash/bashrc.d/git.sh
@@ -6,40 +6,41 @@ PROMPT_DIRTRIM=0
 unset PS1
 
 if [[ ${EUID} == 0 ]]; then
-  PS1='\[\033[01;91m\]\h\[\033[00m\] '             # light red root@host
+  PS1='\[\033[91m\]\h\[\033[00m\] '                 # light red root@host
 else
-  PS1='\[\033[01;32m\]\u@\h\[\033[00m\] '          # green user@host
+  PS1='\[\033[92m\]\u@\h\[\033[00m\] '              # green user@host
 fi
 
-PS1+='\[\033[01;34m\]\w\[\033[00m\]'                # blue current working directory
+PS1+='\[\033[93m\]\t\[\033[00m\] '                  # yellow time
+PS1+='\[\033[94m\]\w\[\033[00m\]'                   # blue current working directory
 PS1+='${GITSTATUS_PROMPT:+ $GITSTATUS_PROMPT}'      # git status (requires promptvars option)
 
 if [[ ${EUID} == 0 ]]; then
-  PS1+='\n\[\033[01;$((32-!$?))m\]\$\[\033[00m\] '  # red/green (success/error) # (root)
+  PS1+='\n\[\033[$((92-!$?))m\]\$\[\033[00m\] '     # red/green (success/error) # (root)
 else
-  PS1+='\n\[\033[01;$((31+!$?))m\]\$\[\033[00m\] '  # green/red (success/error) $ (normal)
+  PS1+='\n\[\033[$((91+!$?))m\]\$\[\033[00m\] '     # green/red (success/error) $ (normal)
 fi
 
-#PS1+='\[\e]0;\u@\h: \w\a\]'                        # terminal title: user@host: dir
+# PS1+='\[\e]0;\u@\h: \w\a\]'                         # terminal title: user@host: dir
 
-PS1+='\[$(RT_CODE="$?";
-tput sc;
-if [[ ${RT_CODE} == 0 ]]; then
-  tput setaf 3;
-  printf "%*s" $((${COLUMNS}-2)) "$(date +%H:%M:%S)";
-else
-  tput setaf 196;
-  printf "%*s" $((${COLUMNS}-11)) "[${RT_CODE}]";
-  tput setaf 3;
-  printf "%*s" 9 "$(date +%H:%M:%S)";
-fi;
-tput sgr0;
-tput rc;
-exit ${RT_CODE})\]'
-
-
+# PS1+='\[$(RT_CODE="$?";
+# tput sc;
+# if [[ ${RT_CODE} == 0 ]]; then
+#   tput setaf 3;
+#   printf "%*s" $((${COLUMNS}-17)) "$(date +%H:%M:%S)";
+# else
+#   tput setaf 196;
+#   printf "%*s" $((${COLUMNS}-26)) "[${RT_CODE}]";
+#   tput setaf 3;
+#   printf "%*s" 9 "$(date +%H:%M:%S)";
+# fi;
+# tput sgr0;
+# tput rc;
+# exit ${RT_CODE})\]'
 
 
 
-## for C in {0..255}; do tput setaf $C; echo -n "$C "; done; tput sgr0; echo
-## for C in {0..255}; do echo -en "\033[$Cm$C\033[00m "; done; echo
+
+
+## for C in {0..255}; do tput setaf ${C}; echo -n "${C} "; done; tput sgr0; echo
+## for C in {0..255}; do echo -en "\033[${C}m${C}\033[00m "; done; echo
